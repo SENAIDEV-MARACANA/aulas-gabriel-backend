b06fb8bf3aee68da1015383172cd714b0bce2b95
anacpp <anacarlacpp@gmail.com>
2025-07-10 18:13:43 -0300

    add folder DB and SQL and query files

---
+++ Turma/ana_carla/DB_and_sql/consulta_cenario_2.sql
@@ -0,0 +1,10 @@
+--Primeiro cadastro, depois marcação
+
+--Cadastro do paciente
+INSERT INTO paciente (nome, telefone)
+VALUES ('Roberto Dias', '(21)91212-3434')
+
+--Cadastro médico
+INSERT INTO medico (nome, especializacao)
+VALUES ('Marcos Almeida', 'Clínico Geral')
+
