6b04e247427b30422306e3fedbe715a92a91959a
anacpp <anacarlacpp@gmail.com>
2025-07-10 18:19:59 -0300

    add query to select id and schedule

--- Turma/ana_carla/DB_and_sql/consulta_cenario_2.sql
+++ Turma/ana_carla/DB_and_sql/consulta_cenario_2.sql
@@ -4,7 +4,14 @@
 INSERT INTO paciente (nome, telefone)
 VALUES ('Roberto Dias', '(21)91212-3434')
 
---Cadastro médico
+--Cadastro médico se não estiver no sistema
 INSERT INTO medico (nome, especializacao)
 VALUES ('Marcos Almeida', 'Clínico Geral')
 
+--Consulta de id 
+SELECT id FROM paciente WHERE nome = 'Roberto Dias';
+SELECT id FROM medico WHERE nome = 'Marcos Almeida';
+
+--agendamento
+INSERT INTO consulta (paciente_id, medico_id, data, hora)
+VALUES (4, 5, '2024-07-15', '14:30');
